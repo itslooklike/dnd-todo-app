4fa3dc9e6abffbd525775754d963b5a4dcc93f5f
a.masalov <delsol@bk.ru>
2019-12-19 21:33:22 +0300

    init

---
+++ src/initialData.js
@@ -0,0 +1,20 @@
+const initialData = {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'kfshjdjhfskjfsdkh' },
+    'task-2': { id: 'task-2', content: 'kfshjdjhfskjfsdkh' },
+    'task-3': { id: 'task-3', content: 'kfshjdjhfskjfsdkh' },
+    'task-4': { id: 'task-4', content: 'kfshjdjhfskjfsdkh' },
+    'task-5': { id: 'task-5', content: 'kfshjdjhfskjfsdkh' },
+    'task-6': { id: 'task-6', content: 'kfshjdjhfskjfsdkh' },
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'TOdo',
+      tasksIds: ['task-1', 'task-2', 'task-3', 'task-4', 'task-5', 'task-6'],
+    },
+  },
+  columnOrder: ['column-1'],
+}
+
+export default initialData
